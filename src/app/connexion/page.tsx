ad014beae759595f0899f6d7a6cea4d6c5f05f1c
QuentinMer <quentin.mercier1603@gmail.com>
2025-05-23 14:09:20 +0200

    modif route

--- src/app/connexion/page.tsx
+++ src/app/connexion/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/auth/login', {
+      const response = await fetch('http://localhost:8000/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/app/inscription/page.tsx
+++ src/app/inscription/page.tsx
@@ -29,7 +29,7 @@ export default function RegisterPage() {
     }
 
     try {
-      const res = await fetch('http://localhost:8000/api/auth/signup', {
+      const res = await fetch('http://localhost:8000/api/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
